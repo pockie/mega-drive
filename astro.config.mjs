d6df856183b9d911edee159b02abeb7a5d43b146
Michael Pokladek <pockienet@hotmail.com>
2023-07-22 00:36:22 +0200

    Deploy to Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://pockie-github.io',
+	base: '/mega-drive',
 	integrations: [mdx(), sitemap()],
 });
